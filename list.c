d195a5f53c4f0f65f61e7ab8b88acc979e9c1b20
mecsys-tornado <isaac@students.ic.unicamp.br>
2011-01-12 19:08:47 -0200

    Pesquisa de lista ligada. Pesquisada em livros, mas implementada por mim.

---
+++ list.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct msg {
+        char *m;
+        };
+
+struct msg *ptrprim, *ptrnovo, *ptratual;
+
+int main()
+{
+        ptrprim = (struct msg *) NULL;
+
+}
