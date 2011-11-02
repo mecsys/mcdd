6d0fdccf5138c70a14578c7aae2c672c48037d3a
Isaac Mechi <isaacmechi@gmail.com>
2011-11-02 13:01:46 -0200

    Alteracao antigas.
    Signed-off-by: Isaac Mechi <isaacmechi@gmail.com>

---
+++ driver/hello.c
@@ -0,0 +1,22 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+
+MODULE_LICENSE("Dual BSD/GPL") /* Evita reclamacoes de kernel */
+
+staticint hellok(void) {
+printk(KERN_ALERT "Kello, Hernel! :]\n");
+
+/* Coisas que fazem os drivers de dispositivos */
+
+return 0;
+}
+
+
+staticvoid byek(void) {
+printk(KERN_ALERT "Bye, Kernel! :[\n");
+}
+
+
+module_init(hellok);
+module_exit(byek);
