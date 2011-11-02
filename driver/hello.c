07a587012564337f3917628e57603ba0c7a0c248
Isaac Mechi <isaacmechi@gmail.com>
2011-11-02 13:02:56 -0200

    Modulo (driver) para estudo.
    Signed-off-by: Isaac Mechi <isaacmechi@gmail.com>

--- driver/hello.c
+++ driver/hello.c
@@ -7,7 +7,7 @@ MODULE_LICENSE("Dual BSD/GPL") /* Evita reclamacoes de kernel */
 staticint hellok(void) {
 printk(KERN_ALERT "Kello, Hernel! :]\n");
 
-/* Coisas que fazem os drivers de dispositivos */
+/* Coisas que fazem os drivers de dispositivos. */
 
 return 0;
 }
